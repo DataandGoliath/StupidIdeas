6d1e658db8f6c9c98e67ac6bfc21281158a23d1b
Nate Jernigan <29870550+DataandGoliath@users.noreply.github.com>
2018-01-25 18:24:35 -0500

    Add files via upload
    BATMAIN!

---
+++ batmain.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int batmain() {
+	int i;
+
+	for(i=0;i<=16;i++) {
+  
+		printf("na ");
+
+	}  
+	printf("BATMAAAAN!\n");
+
+
+	return 0;
+}
+
+int main() {
+	batmain();
+
+	return 0;
+}
