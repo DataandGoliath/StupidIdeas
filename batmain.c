92992be3218becc983536e72df7e564662f93903
Nate Jernigan <29870550+DataandGoliath@users.noreply.github.com>
2018-02-06 23:44:53 +0000

    Update batmain.c

--- batmain.c
+++ batmain.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-//No my formatting doesn't usually look like this. Something about my IDE threw it. 
+
 int batmain() {
 	int i;
-
 	for(i=0;i<=16;i++) {
-  
 		printf("na ");
-
-	}  
+	}
 	printf("BATMAAAAN!\n");
-
-
+	
 	return 0;
 }
-
 int main() {
 	batmain();
-
+	
 	return 0;
 }
